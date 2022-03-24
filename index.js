2ce2004765c8fc39aef4229593a48205b4fee195
NicoMartina <nicolas.martina69@gmail.com>
2022-03-24 11:01:45 +0100

    adding functionality to the start button

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 let firstCard = 9;
 let secondCard = 10;
-let sum = firstCard + secondCard + 5;
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if (sum <= 20) {
-  message = "Do you want to draw another card?";
-} else if (sum === 21) {
-  message = "You got blackjack!";
-  hasBlackJack = true;
-} else if (sum > 21) {
-  message = "sorry buddy";
-  isAlive = false;
+function startGame() {
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw another card?";
+  } else if (sum === 21) {
+    message = "You got blackjack!";
+    hasBlackJack = true;
+  } else if (sum > 21) {
+    message = "sorry buddy";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
-
-console.log(message);
