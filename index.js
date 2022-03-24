d43387804dd0f8dfed5260587d445fd34679c4a8
NicoMartina <nicolas.martina69@gmail.com>
2022-03-24 12:23:57 +0100

    button functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let firstCard = 9;
 let secondCard = 10;
 let sum = firstCard + secondCard;
+let cards = [firstCard, secondCard];
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
@@ -9,7 +10,10 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  renderGame();
+}
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw another card?";
@@ -17,8 +21,16 @@ function startGame() {
     message = "You got blackjack!";
     hasBlackJack = true;
   } else if (sum > 21) {
-    message = "sorry buddy";
+    message = "You're out of the game";
     isAlive = false;
   }
   messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = 7;
+  sum += card;
+  cards.push(card);
+  console.log(cards);
+  renderGame();
 }
